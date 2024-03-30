a8d45e36c927c7ca7f3e0a1ff48dd0e1b2795895
Viktorija Deksne <102534473+vdeksne@users.noreply.github.com>
2024-03-30 22:51:08 +0200

    top menu and burger finished
    still to do:
    - united alignment to left and right
    - bg still on scroll plugin needed when burger menu is opened

--- portfolioFinal/dist/menu.js
+++ portfolioFinal/dist/menu.js
@@ -1,13 +1,23 @@
-const btnOpen = document.querySelector('#btnOpen');
-const btnClose = document.querySelector('#btnClose');
+document.addEventListener('DOMContentLoaded', function() {
+    const topnav = document.querySelector('.topnav');
+    const topnavLinks = document.querySelector('.topnav__links');
+    const logoDesktop = document.querySelector('.logo-desktop');
+    const btnOpen = document.getElementById('btnOpen');
+    let isOpen = false;
 
-function openMobileMenu() {
-    btnOpen.setAttribute('aria-expanded', 'true');
-}
-
-function closeMobileMenu() {
-    btnClose.setAttribute('aria-expanded', 'false');
-}
-
-btnOpen.addEventListener('click', openMobileMenu);
-btnClose.addEventListener('click', closeMobileMenu);
+    topnav.addEventListener('click', function() {
+        if (isOpen) {
+            topnav.style.height = '2.2em'; // Set back to initial height
+            topnavLinks.style.display = 'none'; // Hide the links
+            logoDesktop.style.display = 'flex'; // Show the logo
+            btnOpen.style.display = 'block'; // Show the button
+        } else {
+            topnav.style.height = '100vh'; // Set to full viewport height
+            topnavLinks.style.display = 'block'; // Show the links
+            topnavLinks.style.flexDirection = 'column'; // Set flex-direction to column
+            logoDesktop.style.display = 'none'; // Hide the logo
+            btnOpen.style.display = 'none'; // Hide the button
+        }
+        isOpen = !isOpen; // Toggle isOpen flag
+    });
+});
