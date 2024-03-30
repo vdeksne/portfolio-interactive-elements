0b11aa75e49865a94f36b4f269d5db8542c50449
Viktorija Deksne <102534473+vdeksne@users.noreply.github.com>
2024-03-30 14:04:02 +0200

    menu in progress

---
+++ portfolioFinal/dist/menu.js
@@ -0,0 +1,13 @@
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+
+function openMobileMenu() {
+    btnOpen.setAttribute('aria-expanded', 'true');
+}
+
+function closeMobileMenu() {
+    btnClose.setAttribute('aria-expanded', 'false');
+}
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
