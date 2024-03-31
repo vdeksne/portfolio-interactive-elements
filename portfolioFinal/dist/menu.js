ea8658396e9ae8bf28ef2811ea12c58072663f66
Viktorija Deksne <102534473+vdeksne@users.noreply.github.com>
2024-03-31 16:14:08 +0300

    Error for hamburger menu
    When the menu is opened at 50% scroll it cuts the open state to half size not 100vw

--- portfolioFinal/dist/menu.js
+++ portfolioFinal/dist/menu.js
@@ -6,18 +6,29 @@ document.addEventListener('DOMContentLoaded', function() {
     let isOpen = false;
 
     topnav.addEventListener('click', function() {
-        if (isOpen) {
+        // Define a function to scroll to the top and then expand the menu
+        function scrollToTopAndExpand() {
+            window.scrollTo({ top: 0, behavior: 'smooth' }); // Scroll to top
+            setTimeout(function() {
+                topnav.style.height = '100vh'; // Set to full viewport height
+                topnavLinks.style.display = 'block'; // Show the links
+                topnavLinks.style.flexDirection = 'column'; // Set flex-direction to column
+                logoDesktop.style.display = 'none'; // Hide the logo
+                btnOpen.style.display = 'none'; // Hide the button
+                bodyScrollLockUpgrade.disableBodyScroll(document.body); // Disable body scrolling
+                isOpen = true; // Set isOpen flag to true
+            }, 500); // Adjust the delay as needed
+        }
+
+        if (!isOpen) {
+            scrollToTopAndExpand(); // Call the function to scroll to top and expand the menu
+        } else {
             topnav.style.height = '2.2em'; // Set back to initial height
             topnavLinks.style.display = 'none'; // Hide the links
             logoDesktop.style.display = 'flex'; // Show the logo
             btnOpen.style.display = 'block'; // Show the button
-        } else {
-            topnav.style.height = '100vh'; // Set to full viewport height
-            topnavLinks.style.display = 'block'; // Show the links
-            topnavLinks.style.flexDirection = 'column'; // Set flex-direction to column
-            logoDesktop.style.display = 'none'; // Hide the logo
-            btnOpen.style.display = 'none'; // Hide the button
+            bodyScrollLockUpgrade.enableBodyScroll(document.body); // Enable body scrolling
+            isOpen = false; // Set isOpen flag to false
         }
-        isOpen = !isOpen; // Toggle isOpen flag
     });
 });
